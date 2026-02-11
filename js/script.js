a85fd6c979a9c4ecef8ad0bcb2db2a517fb65e53
dosteng <dosteng2@gmail.com>
2026-02-11 23:14:05 +0700

    Delete js directory

--- js/script.js
+++
@@ -1,8 +0,0 @@
-// Smooth scroll untuk anchor link
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute('href'))
-      .scrollIntoView({ behavior: 'smooth' });
-  });
-});
